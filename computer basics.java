f7ee90bd11524c0be6e05953e062588649939998
BhuvneshAzad <123796734+BhuvneshAzad@users.noreply.github.com>
2023-02-27 01:48:17 +0530

    Add files via upload

---
+++ computer basics.java
@@ -0,0 +1,75 @@
+// Assignment-1
+// Q1. What is a Computer?
+// Ans. A computer is a machine that accepts information (in the form of digitised data) and
+// processes it in accordance with a programme, piece of software, or set of instructions that
+// specify how the information should be handled
+
+// Q2. What is RAM?
+// Ans. Ram stands for Random-access memory. RAM is the main memory in a computer. RAM
+// is a temporary memory bank where your computer stores data it needs to retrieve quickly RAM
+// is volatile. This means that information is kept in RAM while the computer is running, but it is
+// erased when the machine is powered off. The operating system and other files are frequently
+// loaded from an HDD or SSD into RAM when the computer is restarted.
+
+// Q3. Where is data stored in a computer?
+// Ans. Data is stored in various components of a computer system, depending on its type and
+// purpose.
+// Hard Disk Drives (HDD): Data is usually stored in the magnetic platters of the hard disk drive.
+// The HDD stores data even when the computer is turned off.
+// Solid-State Drives (SSD): Data is stored on flash memory chips within an SSD, and it is also
+// stored even when the computer is turned off.
+// Random Access Memory (RAM): Data is stored temporarily in RAM while the computer is
+// running. When the computer is turned off, any data stored in RAM is lost.
+// Cache memory: Data is stored temporarily in cache memory, which is a smaller and faster type
+// of memory that helps to speed up the computer's performance.
+// Optical discs: Data is stored on optical discs like CDs, DVDs, and Blu-ray discs, using a laser
+// to read and write data onto a reflective surface.
+// USB flash drives: Data is stored on flash memory chips in a small and portable USB drive.
+// Cloud storage: Data can also be stored remotely on servers hosted by a cloud storage provider,
+// accessible through the internet.
+// Overall, data can be stored in various locations and formats depending on the requirements of
+// the computer system and its users.
+
+// Q4. What is that input device used to type text and numbers on a document in the computer
+// system?
+// Ans. The input device used to type text and numbers on a document in a computer system is
+// called a keyboard. A keyboard is a device with a set of keys that are used to input letters,
+// numbers, and symbols into a computer. When a key is pressed on the keyboard, it sends an
+// electrical signal to the computer, which interprets the signal and displays the corresponding
+// character on the screen. The keyboard is one of the primary input devices used with a computer,
+// and it is essential for tasks such as writing documents, sending emails, and browsing the web.
+
+// Q5. What are the output devices?
+// Ans. The output is what we get back from the command we gave the computer through the
+// input device. The monitor is the device that is utilised the most since commands are entered
+// via the keyboard, and the outcome is shown on the monitor after processing.
+
+// Q6. Which is the input device that allows a user to move the cursor or pointer on the screen?
+// Ans. The input device that allows a user to move the cursor or pointer on the screen is called a
+// pointing device. The most common pointing device is a mouse, but there are also other types
+// of pointing devices such as touchpads, trackballs, and pointing sticks.
+
+// Q7. Which language is directly understood by the computer without a translation program?
+// Ans. The only language directly understood by a computer without a translation program is
+// machine language, also known as "machine code". Machine language is a low-level
+// programming language consisting of binary code, which is a series of 1's and 0's that represent
+// the instructions and data that a computer can execute.
+
+// Q8. What are Input devices?
+// Ans. Input devices are devices that allow users to input data or instructions into a computer or
+// other electronic device. They are used to communicate with and control a computer system,
+// and they are essential for performing a wide variety of tasks. Some common input devices
+// include:
+// Keyboard - used to type text and numbers into a computer.
+// Mouse - used to navigate the computer screen and perform various functions such as selecting,
+// dragging and dropping.
+// Touchpad - used as a substitute for a mouse, especially in laptops.
+// Trackball - used as a substitute for a mouse, but instead of moving the device, the user moves
+// a ball on top of it.
+// Scanner - used to input images or text from printed pages into a computer system.
+// Microphone - used to input audio into a computer, such as voice commands or for recording
+// audio.
+// Joystick - used for gaming or controlling movement in simulations and other applications.
+// Webcam - used for capturing video and images of oneself or the surrounding environment.
+// Digital Pen or Stylus - used to write or draw on a digital screen, particularly useful in graphics
+// or design applications.
